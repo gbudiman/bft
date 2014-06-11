bb9f5e832e7311f67e86cf0435f30c8439f6dd5d
gbudiman <wahyu.g@gmail.com>
2014-06-11 02:52:49 -0400

    Depth-NGNT Scrap Complete
    NGNT site completely scrapped

--- lib/Scrapper.rb
+++ lib/Scrapper.rb
@@ -6,6 +6,7 @@ require 'nokogiri'
 class Scrapper
 	URI_NGNT = 'http://bravefrontier.nogamenotalk.com'
 	URI_WIKIA = 'http://bravefrontierglobal.wikia.com/wiki'
+	LINK_FILE = File.join(File.dirname(__FILE__), 'links.mp')
 
 	attr_reader :unit_table, :index
 
@@ -36,23 +37,38 @@ class Scrapper
 	# 	:rarity => { :rarity => [] }
 	# 	:cost => { :cost => [] }
 	# 	:bb_type => { :bb_type => [] }
-	# 	:usage => { :id => { :used_by => :count } }
 	# }
 
 	def initialize
 		@agent = Mechanize.new
 		@links = Hash.new
 		@unit_table = Hash.new
+		@index = {
+			name: 		{},
+			element: 	{},
+			rarity: 	{},
+			cost: 		{},
+			bb_type: 	{},
+		}
 	end
 
 	def debug *id
-		id.each do |r|
-			ap r
+		begin
+			@links = MessagePack.unpack(IO.binread LINK_FILE)
+		rescue Errno::ENOENT
+			generate_ngnt_links
+		rescue
+			raise
 		end
+
+		links = Hash.new
+		id.each { |r| links[r] = URI.join(URI_NGNT, @links[r]) }
+		parse_ngnt_pages links
 	end
 
 	def scrap
 		generate_ngnt_links
+		parse_ngnt_pages
 	end
 
 	def to_json
@@ -71,8 +87,9 @@ class Scrapper
 				scrap_ngnt_links_on URI.join(URI_NGNT, nav.text)
 			end
 
-			link_file = File.join(File.dirname(__FILE__), 'links.mp')
-			File.open(link_file, 'wb') { |f| f.write @links.to_msgpack}
+			File.open(LINK_FILE, 'wb') { |f| f.write @links.to_msgpack}
+
+			return self
 		end
 
 		def scrap_ngnt_links_on _url
@@ -90,10 +107,57 @@ class Scrapper
 					cost: 			col[5].to_i,
 					bb_type: 		nil,
 					evolution: 		nil,
+					used_by: 		nil,
 					attacks: 		nil,
 				}
 
 				@links[col[0].to_i] = row.xpath('td[4]/a/@href').text
+			end
+		end
+
+		def parse_ngnt_pages _links = nil
+			(_links ||= @links).each do |id, url|
+				ap url
+				body = Nokogiri::HTML(@agent.get(url).body)
+
+				evo_chain = Array.new
+				req_zel = nil
+				req_units = Hash.new
+				body.xpath('//*/table[@class="units evolution"]/tbody').each do |row|
+					evo_chain = 
+						row.xpath('tr/td[@class="unit--number"]').map { |x| x.text.to_i }
+				end
+
+				body.xpath('//*/div[@class="large-12 columns"]/ul/li').each do |req|
+					if req.text =~ /([\,\d]+) zel/i
+						req_zel = $1.gsub(/\,/, '').to_i
+					else
+						req.xpath('a[1]/@href').text =~ /(\d+)/
+						req_units[$1.to_i] = req.text.split("\u00D7")[-1].to_i
+					end
+				end
+
+				last = evo_chain.length - 1
+				(0..last).each do |i|
+					@unit_table[evo_chain[i]] ||= Hash.new
+					next unless @unit_table[evo_chain[i]][:evolution] == nil
+					@unit_table[evo_chain[i]][:evolution] = {
+						pre: 			(i-1<0    ? nil : evo_chain[i-1]),
+						post: 			(i+1>last ? nil : evo_chain[i+1]),
+						requirement: 	nil,
+					}
+				end
+
+				@unit_table[id][:evolution][:requirement] = {
+					zel: 		req_zel,
+					units: 		req_units.dup
+				} if evo_chain.length > 0
+
+				body.xpath('//*/table[@class="units"]/tbody').each do |usage|
+					@unit_table[id] ||= Hash.new
+					@unit_table[id][:used_by] = 
+						usage.xpath('tr/td[@class="unit--number"]').map { |x| x.text.to_i }
+				end
 			end
 		end
 end
--- tests/main_spec.rb
+++ tests/main_spec.rb
@@ -7,16 +7,16 @@ describe Scrapper do
 
 	it { is_expected.to respond_to :unit_table }
 	it { is_expected.to respond_to :index }
-	
+
 	context 'debugging utility' do
 		it 'should allow singular unit ID query' do
-			@scrapper.debug 5
-			expect(@scrapper.unit_table[5]).not_to be nil
+			@scrapper.debug 171
+			expect(@scrapper.unit_table[171]).not_to be nil
 		end
 
 		it 'should allow multiple unit ID query' do
-			@scrapper.debug (20..32).to_a
-			(20..32).each do |id|
+			@scrapper.debug *5..8, 339
+			[5,6,7,8,339].each do |id|
 				expect(@scrapper.unit_table[id]).not_to be nil
 			end
 		end
@@ -38,16 +38,6 @@ describe Scrapper do
 			end
 		end
 
-		it 'should only have 6 kinds of elements' do
-			captured_elements = Hash.new
-
-			@scrapper.unit_table.each do |id, value|
-				captured_elements[value[:element]] = 1
-			end
-
-			expect(captured_elements.length).to be 6
-		end
-
 		it 'should generate links.mp file' do
 			expect(File.exists? @link_file_path).to be true
 		end
@@ -60,7 +50,7 @@ describe Scrapper do
 		end
 
 		it 'should be dump-able' do
-			#@scrapper.to_s
+			@scrapper.to_s
 		end
 	end
 end
